091815a7fc43d9cdb90a970e42acccbee4e91297
Mohamed Elgazzar <momoelgazzar@gmail.com>
2024-10-09 19:24:22 +0200

    Adapted Seed to match new cahnges

--- db/schema.rb
+++ db/schema.rb
@@ -76,9 +76,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_09_142428) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.string "first_name"
     t.string "last_name"
-    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
