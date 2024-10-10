4a07df5f63863898c1f397040c77f270748e0602
Pablo Guelfi <pguelfi@gmail.com>
2024-10-10 12:20:09 +0200

    fixed schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Instrument.destroy_all
+User.destroy_all          # Clear bookings first (if applicable)
 
 user = User.create!(
   first_name: 'Sample First Name',
@@ -17,6 +19,6 @@ user = User.create!(
 )
 
 Instrument.create!([
-  { name: 'Guitar', description: 'An acoustic guitar.', capacity: 1, address: '123 Music Lane', price: 100.0, user_id: user.id },
-  { name: 'Piano', description: 'A grand piano.', capacity: 2, address: '456 Melody St.', price: 200.0, user_id: user.id }
+  { name: 'Guitar', description: 'An acoustic guitar.', address: '123 Music Lane', price: 100.0, user_id: user.id },
+  { name: 'Piano', description: 'A grand piano.', address: '456 Melody St.', price: 200.0, user_id: user.id }
 ])
