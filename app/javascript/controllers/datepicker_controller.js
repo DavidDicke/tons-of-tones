06233e76d816d8fe7a16ec3f929e0335dc05c2cc
Pablo Guelfi <pguelfi@gmail.com>
2024-10-10 17:26:04 +0200

    replaced start_date and end_date attributes of Booking with dates_range

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,16 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr";
-
+// Connects to data-controller="datepicker"
 export default class extends Controller {
   static values = {
-    unavailableDates: Array
+    unavailableDates: Array // Declare a value to receive unavailable dates
   };
 
   connect() {
     flatpickr(this.element, {
+      mode: "multiple",
       minDate: "today",
       dateFormat: "Y-m-d",
-      disable: this.unavailableDatesValue
+      disable: this.unavailableDatesValue // Use the unavailable dates passed to the controller
     });
   }
 }
