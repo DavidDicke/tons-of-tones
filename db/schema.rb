9f385db5b5b2136ced03e87cd3edb480ff7f1b55
Mohamed Elgazzar <momoelgazzar@gmail.com>
2024-10-12 19:23:06 +0200

    Install Active Storage and run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -64,6 +64,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_11_095458) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+    t.integer "capacity"
     t.index ["user_id"], name: "index_instruments_on_user_id"
   end
 
@@ -75,9 +76,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_11_095458) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.string "first_name"
     t.string "last_name"
-    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
