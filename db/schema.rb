8534112099ca54bd597b772c39d195b0a8ce969d
David Dicke <post@david-dicke.de>
2024-10-13 23:55:05 +0200

    created homepage including upgraded navbar with menu that can be toggled as modal

--- db/schema.rb
+++ db/schema.rb
@@ -63,7 +63,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_11_095458) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "description"
     t.index ["user_id"], name: "index_instruments_on_user_id"
   end
 
@@ -77,7 +76,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_11_095458) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
