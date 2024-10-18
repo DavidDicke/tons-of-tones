1e185f7583e1f3dc208c6e17d9116a00be584d0a
Pablo Guelfi <pguelfi@gmail.com>
2024-10-18 18:02:58 +0200

    front end booking views

--- app/models/user_review.rb
+++ app/models/user_review.rb
@@ -1,6 +1,7 @@
 class UserReview < ApplicationRecord
   belongs_to :user
   belongs_to :booking
+  has_one :user, through: :booking
 
   validates :rating, presence: true
   validates :content, presence: true
