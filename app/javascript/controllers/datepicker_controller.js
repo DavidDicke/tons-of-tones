87e273b86373cd23590ea773754be169da6c9ea4
David Dicke <post@david-dicke.de>
2024-10-23 00:36:34 +0200

    updated flatpickr

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -5,14 +5,19 @@ export default class extends Controller {
   static targets = ["startDate", "endDate"]
   static values = { bookings: Array }
   connect() {
-    if(this.bookingsValue.length !== 0){
-      flatpickr(this.startDateTarget, { minDate: "today", disable: this.bookingsValue });
-      flatpickr(this.endDateTarget, { minDate: "today", disable: this.bookingsValue });
-    } else {
-      flatpickr(this.startDateTarget, { minDate: "today" });
-      flatpickr(this.endDateTarget, { minDate: "today" });
-      console.log(this.startDateTarget, this.endDateTarget);
+    const startDatePicker = flatpickr(this.startDateTarget, {
+      minDate: "today",
+      disable: this.bookingsValue,
+      disableMobile: "true",
+      onChange: (selectedDates, dateStr) => {
+        this.endDateFlatpickr.set('minDate', dateStr);
+      }
+    });
 
-    }
+    this.endDateFlatpickr = flatpickr(this.endDateTarget, {
+      minDate: "today",
+      disable: this.bookingsValue,
+      disableMobile: "true"
+    });
   }
 }
