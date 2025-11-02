07a9c9dc607ce5ce96906b1ed0c4f92a4c47da2d
David Dicke <170459648+DavidDicke@users.noreply.github.com>
2025-11-02 22:24:05 +0100

    change enum

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,7 +4,7 @@ class Booking < ApplicationRecord
   has_one :booking_review, dependent: :destroy
   has_one :user_review, dependent: :destroy
 
-  enum status: { pending: 1, confirmed: 2, cancelled: 3, completed: 4, confirm_viewed: 5 }
+  enum :status, { pending: 1, confirmed: 2, cancelled: 3, completed: 4, confirm_viewed: 5 }
   validates :start_date, presence: true
   validates :end_date, presence: true
 end
