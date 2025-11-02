df46e6fdcfe4ba62f27d66400fc70883b8bda362
David Dicke <170459648+DavidDicke@users.noreply.github.com>
2025-11-02 22:28:13 +0100

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_20_092614) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_02_210339) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.bigint "record_id", null: false
+    t.string "record_type", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
     t.bigint "byte_size", null: false
     t.string "checksum"
+    t.string "content_type"
     t.datetime "created_at", null: false
+    t.string "filename", null: false
+    t.string "key", null: false
+    t.text "metadata"
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
@@ -43,64 +43,64 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_20_092614) do
   end
 
   create_table "booking_reviews", force: :cascade do |t|
-    t.text "content"
-    t.integer "rating"
-    t.bigint "user_id", null: false
     t.bigint "booking_id", null: false
+    t.text "content"
     t.datetime "created_at", null: false
+    t.integer "rating"
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["booking_id"], name: "index_booking_reviews_on_booking_id"
     t.index ["user_id"], name: "index_booking_reviews_on_user_id"
   end
 
   create_table "bookings", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.date "end_date"
+    t.bigint "instrument_id", null: false
+    t.date "start_date"
     t.integer "status", default: 1
     t.integer "total_price"
-    t.bigint "instrument_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "start_date"
-    t.date "end_date"
+    t.bigint "user_id", null: false
     t.index ["instrument_id"], name: "index_bookings_on_instrument_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "instruments", force: :cascade do |t|
-    t.string "name"
     t.string "address"
     t.string "category"
-    t.integer "price"
-    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.text "description"
     t.float "latitude"
     t.float "longitude"
+    t.string "name"
+    t.integer "price"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_instruments_on_user_id"
   end
 
   create_table "user_reviews", force: :cascade do |t|
-    t.text "content"
-    t.integer "rating"
-    t.bigint "user_id", null: false
     t.bigint "booking_id", null: false
+    t.text "content"
     t.datetime "created_at", null: false
+    t.integer "rating"
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["booking_id"], name: "index_user_reviews_on_booking_id"
     t.index ["user_id"], name: "index_user_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
